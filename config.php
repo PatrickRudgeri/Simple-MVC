a45c54dda45c751695d92b69cdd366d7650c8979
Patrick Rudgeri <patrick.rudgeri@ice.ufjf.br>
2020-06-13 20:24:44 -0300

    Update config.php

--- config.php
+++ config.php
@@ -14,8 +14,8 @@ if (ENVIRONMENT == 'development') {
 	$config['dbuser'] = 'root';
 	$config['dbpass'] = '123';
 } else {
-	$config['dbname'] = 'curso_php';
-	$config['host'] = 'localhost';
-	$config['dbuser'] = 'root';
-	$config['dbpass'] = '123';
+	$config['dbname'] = 'd3cbc4gffso5pm';
+	$config['host'] = 'ec2-34-204-22-76.compute-1.amazonaws.com:5432';
+	$config['dbuser'] = 'dvdrdxnegedove';
+	$config['dbpass'] = '5d279492c468e3e9644c3e21fd700ecf1b1c94c2c369ea9973fba23377b1565e';
 }
